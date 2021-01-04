5a35d6dca29e63c6d7bb46fb77ba2c32398f6698
Yan <yan25.1996@gmail.com>
2021-01-04 16:55:28 -0300

    04-01-2021
    Sistema de slides / arreglar menu lateral en version movil

---
+++ proyecto-v3/assets/js/main.js
@@ -0,0 +1,6 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'], 
+    /* lazyLoad: true */
+});
